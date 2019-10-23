0355583dcc68670f6820b507197f437dd1622ea1
Jacob Rivera <edjacob25@gmail.com>
2019-10-22 20:12:11 -0500

    Moved extensions to other file

--- MeasuresComparator/Program.cs
+++ MeasuresComparator/Program.cs
@@ -257,14 +257,5 @@ namespace MeasuresComparator
         }
     }
 
-    public static class Extensions
-    {
-        public static IEnumerable<IEnumerable<T>> DifferentCombinations<T>(this IEnumerable<T> elements, int k)
-        {
-            return k == 0
-                ? new[] { new T[0] }
-                : elements.SelectMany((e, i) =>
-                    elements.Skip(i + 1).DifferentCombinations(k - 1).Select(c => new[] { e }.Concat(c)));
-        }
-    }
+
 }
--- MeasuresComparator/Utils.cs
+++ MeasuresComparator/Utils.cs
@@ -1,8 +1,77 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MeasuresComparator
 {
     internal class ParserException : Exception
     {
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<T>> DifferentCombinations<T>(this IEnumerable<T> elements, int k)
+        {
+            return k == 0
+                ? new[] { new T[0] }
+                : elements.SelectMany((e, i) =>
+                    elements.Skip(i + 1).DifferentCombinations(k - 1).Select(c => new[] { e }.Concat(c)));
+        }
+
+        /// <summary>
+        /// Returns the row with number 'row' of this matrix as a 1D-Array.
+        /// </summary>
+        public static T[] GetRow<T>(this T[,] matrix, int row)
+        {
+            var rowLength = matrix.GetLength(1);
+            var rowVector = new T[rowLength];
+
+            for (var i = 0; i < rowLength; i++)
+                rowVector[i] = matrix[row, i];
+
+            return rowVector;
+        }
+
+
+
+        /// <summary>
+        /// Sets the row with number 'row' of this 2D-matrix to the parameter 'rowVector'.
+        /// </summary>
+        public static void SetRow<T>(this T[,] matrix, int row, T[] rowVector)
+        {
+            var rowLength = matrix.GetLength(1);
+
+            for (var i = 0; i < rowLength; i++)
+                matrix[row, i] = rowVector[i];
+        }
+
+
+
+        /// <summary>
+        /// Returns the column with number 'col' of this matrix as a 1D-Array.
+        /// </summary>
+        public static T[] GetCol<T>(this T[,] matrix, int col)
+        {
+            var colLength = matrix.GetLength(0);
+            var colVector = new T[colLength];
+
+            for (var i = 0; i < colLength; i++)
+                colVector[i] = matrix[i, col];
+
+            return colVector;
+        }
+
+
+
+        /// <summary>
+        /// Sets the column with number 'col' of this 2D-matrix to the parameter 'colVector'.
+        /// </summary>
+        public static void SetCol<T>(this T[,] matrix, int col, T[] colVector)
+        {
+            var colLength = matrix.GetLength(0);
+
+            for (var i = 0; i < colLength; i++)
+                matrix[i, col] = colVector[i];
+        }
     }
 }
